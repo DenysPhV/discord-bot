86d5d7991f434e4c16644a3d8151d8535b01c6e8
Denys Filichkin <denys.philichkin@gmail.com>
2024-04-01 13:08:47 +0300

    save

--- Dockerfile
+++ Dockerfile
@@ -3,10 +3,36 @@ FROM python:3.12
 ENV PYTHONUNBUFFERED True
 
 WORKDIR /bot
+
 COPY . ./
 
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
 CMD ["python", "/bot/main.py"]
+
+
+#========================
+# Selenium Hub Configuration
+#========================
+
+EXPOSE 4442
+EXPOSE 4443
+EXPOSE 4444
+
+# In seconds, maps to "--session-request-timeout"
+ENV SE_SESSION_REQUEST_TIMEOUT 300
+# In seconds, maps to "--session-retry-interval"
+ENV SE_SESSION_RETRY_INTERVAL 15
+# In seconds, maps to "--healthcheck-interval"
+ENV SE_HEALTHCHECK_INTERVAL 120
+# Boolean value, maps "--relax-checks"
+ENV SE_RELAX_CHECKS true
+
+COPY --chown="${SEL_UID}:${SEL_GID}" start-selenium-grid-hub.sh \
+    /opt/bin/
+
+COPY selenium-grid-hub.conf /etc/supervisor/conf.d/
+
+ENV SE_OTEL_SERVICE_NAME "selenium-hub"
 
--- main.py
+++ main.py
@@ -49,11 +49,12 @@ async def start(ctx):
     session.start_time = ctx.message.created_at.timestamp()
     human_readable_time = ctx.message.created_at.strftime("%H:%M:%S")
     break_reminder.start()
-    await ctx.send(f"New session started at {human_readable_time}")
-    await ctx.send(
-        "If you want to registration on the site use the command '>registration' and entered <your email>,  <password min 12 "
-        "simbols> and "
-        "<confirm password>.")
+    await ctx.send(f"New session started at {human_readable_time}\n")
+    await ctx.send("If you want to registration on the site, use the command \n "
+                   "**>registration** "
+                   "<**your email**> "
+                   "<**password min 12 simbols**> "
+                   "<**confirm password**>")
 
 
 @bot.command()
@@ -78,27 +79,6 @@ async def registration(ctx, email, password, confirm_password):
     await ctx.send("Registration failed. Please try again.")
 
 
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-    if message.content == 'hello':
-        response = "Hello, I'm a bot!"
-        await message.channel.send(response)
-    elif message.content.startswith('pokemon'):
-        pokemon_name = message.content[len('pokemon'):].strip()
-        if pokemon_name:
-            req = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon_name.lower()}")
-            if req.status_code == 200:
-                pokemon_data = req.json()
-                pokemon_id = pokemon_data.get('id')
-                pokemon_type = pokemon_data.get('types')[0].get('type').get('name')
-                await message.channel.send(
-                    f"Pokemon found!\nName: {pokemon_name.capitalize()}\nID: {pokemon_id}\nType: {pokemon_type}")
-            else:
-                await message.channel.send(f"Sorry, I couldn't find information about the pokemon '{pokemon_name}'.")
-        else:
-            await message.channel.send("Please provide the name of the Pokemon after the 'pokemon' keyword.")
+if __name__ == '__main__':
+    bot.run(settings.DISCORD_TOKEN)
 
-
-bot.run(settings.DISCORD_TOKEN)
